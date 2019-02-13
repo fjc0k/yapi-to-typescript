c34c6734f2350eb44ad9a7970e8e3d3b1bc0bc5b
Jay Fong <fjc0kb@gmail.com>
2019-02-13 17:53:15 +0800

    chore: 修正一处拼写错误

--- src/propDefinitionsToJsonSchema.ts
+++ src/propDefinitionsToJsonSchema.ts
@@ -4,19 +4,19 @@ import { PropDefinitions } from './types'
 /**
  * 将属性定义转换为 json-schema。
  *
- * @param propDefinitionsefinitions 要转换的属性定义列表
+ * @param propDefinitions 要转换的属性定义列表
  * @returns 符合 JSONSchema4 格式的对象
  */
-export default function propDefinitionsToJsonSchema(propDefinitionsefinitions: PropDefinitions): JSONSchema4 {
+export default function propDefinitionsToJsonSchema(propDefinitions: PropDefinitions): JSONSchema4 {
   return {
     type: 'object',
-    required: propDefinitionsefinitions.reduce((res, prop) => {
+    required: propDefinitions.reduce((res, prop) => {
       if (prop.required) {
         res.push(prop.name)
       }
       return res
     }, []),
-    properties: propDefinitionsefinitions.reduce<JSONSchema4['properties']>((res, prop) => {
+    properties: propDefinitions.reduce<JSONSchema4['properties']>((res, prop) => {
       res[prop.name] = {
         type: prop.type,
         description: prop.comment,
