d9f7c763821610579e0274b492c9d74d57a3aba2
Jay Fong <fjc0kb@gmail.com>
2019-04-28 10:35:23 +0800

    feat(typing): parseRequestData 可接受 undefined 参数

--- src/helpers.ts
+++ src/helpers.ts
@@ -26,14 +26,14 @@ export class FileData<T = any> {
 /**
  * 解析请求数据，从请求数据中分离出普通数据和文件数据。
  *
- * @param requestData 要解析的请求数据
+ * @param [requestData] 要解析的请求数据
  * @returns 包含普通数据(data)和文件数据(fileData)的对象，data、fileData 为空对象时，表示没有此类数据
  */
 export function parseRequestData<
   RD extends { [key: string]: any },
   DK extends { [K in keyof RD]: RD[K] extends FileData ? never : K }[keyof RD],
   FDK extends { [K in keyof RD]: RD[K] extends FileData ? K : never }[keyof RD]
->(requestData: RD): {
+>(requestData?: RD): {
   data: { [K in DK]: RD[K] },
   fileData: { [K in FDK]: RD[K] extends FileData<infer T> ? T : any },
 } {
