a2375ea7483c2aab506cd9479067d235a0feab17
Jay Fong <fjc0kb@gmail.com>
2019-04-29 13:13:47 +0800

    tests: 增加对 cli 的测试

---
+++ tests/cli.test.ts
@@ -0,0 +1,37 @@
+import fs from 'fs-extra'
+import path from 'path'
+import { sleep } from 'vtils'
+
+const targetDir = path.join(__dirname, '.tmpTest')
+const generatedConfigFile = path.join(targetDir, 'ytt.config.ts')
+const generatedApiFile = path.join(targetDir, '/src/api/index.ts')
+const generatedRequestFile = path.join(targetDir, '/src/api/request.ts')
+
+const argvCount = process.argv.length
+
+describe('cli', () => {
+  test('正确初始化配置文件 & 生成结果', async () => {
+    fs.ensureDirSync(targetDir)
+    process.chdir(targetDir)
+
+    process.argv.push('init')
+    await import('../src/cli')
+    await sleep(1000)
+    expect(fs.readFileSync(generatedConfigFile).toString()).toMatchSnapshot()
+
+    jest.resetModules()
+    process.argv.splice(argvCount, process.argv.length)
+    fs.writeFileSync(
+      generatedConfigFile,
+      fs.readFileSync(generatedConfigFile).toString()
+        .replace('yapi-to-typescript', '../../src')
+        .replace(`dataKey: 'data',`, '')
+        .replace(`id: 50,`, `id: 58,`),
+    )
+    await import('../src/cli')
+    await sleep(2000)
+    expect(fs.readFileSync(generatedApiFile).toString()).toMatchSnapshot()
+    expect(fs.readFileSync(generatedRequestFile).toString()).toMatchSnapshot()
+    fs.removeSync(targetDir)
+  })
+})
