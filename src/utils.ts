c970bca9d95e0802ad0815ca3980fe4dda366179
yanetut <amateuryy@gmail.com>
2020-02-11 11:45:28 +0800

    fix: ignore jsonSchema maxItems, minItems

--- src/utils.ts
+++ src/utils.ts
@@ -54,6 +54,8 @@ export function processJsonSchema<T extends JSONSchema4>(jsonSchema: T): T {
   // 去除 title 和 id，防止 json-schema-to-typescript 提取它们作为接口名
   delete jsonSchema.title
   delete jsonSchema.id
+  delete jsonSchema.minItems
+  delete jsonSchema.maxItems
 
   // 将 additionalProperties 设为 false
   jsonSchema.additionalProperties = false
