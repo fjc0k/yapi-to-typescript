72a5b4434a238de1997da5e510dd2ba9d8317d07
Jay Fong <fjc0kb@gmail.com>
2021-05-19 10:19:56 +0800

    chore: 更新 logo

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'dumi'
 export default defineConfig({
   title: 'YTT',
   description: '一个 YApi 代码生成工具',
+  favicon:
+    'https://cdn.jsdelivr.net/gh/fjc0k/yapi-to-typescript/assets/logo.png',
   logo: 'https://cdn.jsdelivr.net/gh/fjc0k/yapi-to-typescript/assets/logo.png',
   locales: [['zh-CN', '中文']],
   outputPath: 'docs/dist',
