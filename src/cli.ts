1aefd417e4a235a23569510beb419efb32f2b2b3
Jay Fong <fjc0kb@gmail.com>
2021-12-21 15:57:57 +0800

    feat: 通过 ytt init 初始化的配置文件中的 getRequestFunctionName 默认改为「以接口全路径生成请求函数名」并备注遇到请求函数名是语法关键词报错、重复时的解决方案

--- src/cli.ts
+++ src/cli.ts
@@ -131,9 +131,14 @@ export async function run(
                   {
                     id: 0,
                     getRequestFunctionName(interfaceInfo, changeCase) {
-                      return changeCase.camelCase(
-                        interfaceInfo.parsedPath.name,
-                      )
+                      // 以接口全路径生成请求函数名
+                      return changeCase.camelCase(interfaceInfo.path)
+
+                      // 若生成的请求函数名存在语法关键词报错、或想通过某个关键词触发 IDE 自动引入提示，可考虑加前缀，如:
+                      // return changeCase.camelCase(\`api_\${interfaceInfo.path}\`)
+
+                      // 若生成的请求函数名有重复报错，可考虑将接口请求方式纳入生成条件，如:
+                      // return changeCase.camelCase(\`\${interfaceInfo.method}_\${interfaceInfo.path}\`)
                     },
                   },
                 ],
